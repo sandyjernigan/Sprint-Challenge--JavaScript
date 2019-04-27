2fb712c415f53bf9179c5a004c367f1c320770ab
sandyjernigan <sandy.jernigan@gmail.com>
2019-04-27 12:40:49 -0400

    Given the following information about dinosaurs, create 3 objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,10 +7,31 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyrannosaurus = {
+  "name": "tyrannosaurus",
+  "diet": "carnivorous",
+  "weight": "7000 kg",
+  "length": "12m",
+  "period": "Late Cretaceous"
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus = {
+  "name": "stegosaurus",
+  "diet": "herbivorous",
+  "weight": "2000 kg",
+  "length": "9m",
+  "period": "Late Jurassic"
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const velociraptor = {
+  "name": "velociraptor",
+  "diet": "carnivorous",
+  "weight": "15 kg",
+  "length": "1.8m",
+  "period": "Late Cretaceous"
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
